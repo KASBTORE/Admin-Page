451237565878de8ea7c711beacbe071cc72d48ff
Umutonimireille <mireilleumutoni05@gmail.com>
2023-04-19 11:34:55 +0200

    new changes

--- kabstore/vite.config.js
+++ kabstore/vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000, // Set custom chunk size limit to 1000 kB
+  },
+  
   plugins: [react()],
   
 })
